b8325359b3c508381d53e8f44d99807efefdf9ae
Reshma66-web <reshma88450@gmail.com>
2026-05-20 07:51:33 +0530

    tm1

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,5 +5,5 @@ test("login module",async({page})=>{
     await page.locator('#user-name').fill('standard_user')
     await page.locator('#password').fill('secret_sauce')
     await page.locator('#login-button').click()
-    // team member 1 - 1
+    // team member 1 - 1(first)
 })
